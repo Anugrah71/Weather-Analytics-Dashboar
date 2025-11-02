088b89a9f9dd56967b984b95c78a2b4e6e342d2b
ANUGRAH K <162654910+Anugrah71@users.noreply.github.com>
2025-11-02 23:00:42 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: 5173,
-    proxy: { "/api": "http://localhost:4000" },
+    proxy: { "/api": "https://weather-analytics-dashboar.vercel.app/" },
   },
   plugins: [react()],
 });
